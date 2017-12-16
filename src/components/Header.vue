21cc8407728756af36fc13a9518660a3ff1d2e14
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-16 14:06:38 +1100

    Sass
    Add sass to project.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <header>
+  <header class="site-header">
     <a href="http://www.afl.com.au/" target="_blank" rel="noopener" class="logo"><img src="../assets/logo-afl.svg" width="120" height="auto" alt="AFL"></a>
-    <nav>
-      <ul>
+    <nav class="nav">
+      <ul class="nav__ul">
         <li>
           <router-link :to="'/'">Player ratings</router-link>
         </li>
@@ -20,8 +20,15 @@ export default {
 }
 </script>
 
-<style scoped>
-  header {
+<style lang="scss" scoped>
+  .site-header {
     background-color: #eee;
+  }
+
+  .nav {
+    &__ul {
+      padding-left: 0;
+      list-style-type: none;
+    }
   }
 </style>
