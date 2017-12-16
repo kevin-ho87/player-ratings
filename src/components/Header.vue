6b212d59de344de133e1b45c9520a94bf1185185
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2017-12-16 15:22:18 +1100

    Header style
    Add header styling.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,75 @@
 <template>
   <header class="site-header">
-    <a href="http://www.afl.com.au/" target="_blank" rel="noopener" class="logo"><img src="../assets/logo-afl.svg" width="120" height="auto" alt="AFL"></a>
-    <nav class="nav">
-      <ul class="nav__ul">
-        <li>
-          <router-link :to="'/'">Player ratings</router-link>
-        </li>
-        <li>
-          <router-link :to="'clubs'">Clubs</router-link>
-        </li>
-      </ul>
-    </nav>
+    <div class="site-header__container container">
+      <div class="col">
+        <a href="http://www.afl.com.au/" target="_blank" rel="noopener" class="logo"><img src="../assets/logo-afl.svg" width="120" height="auto" alt="AFL"></a>
+      </div>
+      <div class="col">
+        <nav class="nav">
+          <ul class="nav__ul">
+            <li class="nav__li">
+              <router-link :to="'/'" class="nav__link">Player ratings</router-link>
+            </li>
+            <li class="nav__li">
+              <router-link :to="'clubs'" class="nav__link">Clubs</router-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  // name: 'Header'
+  name: 'Header'
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/base/settings";
+
   .site-header {
-    background-color: #eee;
+    background-color: $primary-color;
+    padding-top: .5rem;
+    padding-bottom: .5rem;
+
+    &__container {
+      align-items: center;
+    }
   }
 
   .nav {
     &__ul {
+      display: flex;
+      margin-top: 0;
+      margin-bottom: 0;
       padding-left: 0;
       list-style-type: none;
+    }
+
+    &__li {
+
+    }
+
+    &__link {
+      display: block;
+      font-size: 1.2rem;
+      padding: 1rem 1.5rem;
+      color: $white;
+      text-decoration: none;
+      font-weight: 700;
+      border-top: 4px solid transparent;
+      border-bottom: 4px solid transparent;
+
+      &:hover {
+        border-bottom-color: $white;
+      }
+    }
+
+    .router-link-exact-active {
+      background-color: $white;
+      color: $primary-color;
     }
   }
 </style>
